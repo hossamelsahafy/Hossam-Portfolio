82e16686f1e3415504076e734843b58090f804ff
HossamElsahafy <hossamelsahafy@localhost.localdomain>
2026-02-05 00:06:52 +0200

    Files

--- app/api/commits.js
+++ app/api/commits.js
@@ -1,27 +1,33 @@
-const BASE_URL = "https://api.github.com";
+const BASE_URL = "https://api.github.com/graphql";
 const TOKEN = process.env.GITHUB_TOKEN;
 const USERNAME = process.env.USERNAME;
 
 export default async function handler(req, res) {
-  try {
-    const response = await fetch(
-      `${BASE_URL}/search/commits?q=author:${USERNAME}`,
-      {
-        headers: {
-          Authorization: `token ${TOKEN}`,
-          Accept: "application/vnd.github.cloak-preview",
-        },
-      },
-    );
-
-    if (!response.ok) {
-      return res.status(500).json({ error: "Failed to fetch commits" });
+  const query = `
+    query {
+      user(login: "${USERNAME}") {
+        contributionsCollection {
+          totalCommitContributions
+        }
+      }
     }
+  `;
 
-    const data = await response.json();
+  try {
+    const response = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        Authorization: `bearer ${TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query }),
+    });
+
+    const result = await response.json();
 
     return res.status(200).json({
-      totalCommits: data.total_count,
+      totalCommits:
+        result.data.user.contributionsCollection.totalCommitContributions,
     });
   } catch (error) {
     return res.status(500).json({ error: error.message });
